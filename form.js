f7701ff8f5238aee5ce8f8f597440dce4710ad09
charlotte <charlottemackenzi@gmail.com>
2023-11-14 00:54:17 +1300

    basic form work

---
+++ form.js
@@ -0,0 +1,136 @@
+// this sucks LMAO
+
+var json = {};
+(async function () {
+
+const fetchJson = await fetch('alts.json');
+json = await fetchJson.json();
+
+var game = document.getElementById('game');
+
+console.log(json);
+for (const [key, value] of Object.entries(json)) {
+	var option = new Option(key, key);
+	game.appendChild(option);
+}
+
+game.addEventListener('change', function() {
+	updateChars();
+});
+
+for(i = 1; i <= 8; i++) {
+	var div = document.createElement('div'); 
+	div.id = "player" + i;
+	div.className = "playerForm";
+	div.innerHTML = `
+		Player ${i}
+		<br>
+		<label for="player${i}name">name: </label>
+		<input type="text" name="player${i}name">
+		<br>
+		<label for="player${i}twt">twitter: </label>
+		<input type="text" name="player${i}twt">
+		<br>
+		<label for="player${i}char">main char: </label>
+		<select name="player${i}char" id="player${i}char"></select>
+		<br>
+		<label for="player${i}alt">main char alt: </label>
+		<select name="player${i}alt" id="player${i}alt"></select>
+		<br>
+		<button onclick="addSecondaryChar(${i})">add secondary character</button>
+		<div id="player${i}secondary">
+		</div>
+	`;
+	document.getElementById("playerFormContainer").appendChild(div);
+}
+
+updateChars();
+
+})();
+
+var currentGame = "";
+
+function addSecondaryChar(i) {
+	var div = document.createElement('div'); 
+	var secondaryCount = document.getElementById("player" + i + "secondary").childElementCount;
+	div.id = "player" + i + "secondary" + secondaryCount;
+	div.className = "secondaryChar";
+	div.innerHTML = `
+		<select name="player${i}secondary${secondaryCount}char" id="player${i}secondary${secondaryCount}char"></select>
+		<select name="player${i}secondary${secondaryCount}alt" id="player${i}secondary${secondaryCount}alt"></select>
+		<button onclick="removeSecondaryChar(${i}, ${secondaryCount})">remove</button>
+	`;
+	document.getElementById("player" + i + "secondary").appendChild(div);
+
+	var char = document.getElementById("player" + i + "secondary" + secondaryCount + "char");
+	char.innerHTML = "";
+	var option = new Option("none", "none");
+	char.appendChild(option);
+	var game = document.getElementById('game').value;
+	for (const [key, value] of Object.entries(json[game])) {
+		var option = new Option(key, key);
+		char.appendChild(option);
+	}
+	char.addEventListener('change', function() {
+		updateAlts(document.getElementById("player" + i + "secondary" + secondaryCount + "char").value, document.getElementById("player" + i + "secondary" + secondaryCount + "alt"));
+	});
+
+	var alt = document.getElementById("player" + i + "secondary" + secondaryCount + "alt");
+	alt.innerHTML = "";
+	var option = new Option("none", "none");
+	alt.appendChild(option);
+	var game = document.getElementById('game').value;
+	if(json[game][char]) {
+		for (const [key, value] of Object.entries(json[game][char])) {
+			var option = new Option(value, value);
+			alt.appendChild(option);
+		}
+	}
+}
+
+function removeSecondaryChar(i, j) {
+	document.getElementById("player" + i + "secondary").removeChild(document.getElementById("player" + i + "secondary" + j));
+}
+
+function updateChars() {
+	for(let i = 1; i <= 8; i++) {
+		for(let j = 0; j < document.getElementById("player" + i + "secondary").childElementCount; j++) {
+			var char = document.getElementById("player" + i + "secondary" + j + "char");
+			char.innerHTML = "";
+			var option = new Option("none", "none");
+			char.appendChild(option);
+			var game = document.getElementById('game').value;
+			for (const [key, value] of Object.entries(json[game])) {
+				var option = new Option(key, key);
+				char.appendChild(option);
+			}
+		}
+		if(currentGame == document.getElementById('game').value) continue;
+		var char = document.getElementById("player" + i + "char");
+		char.innerHTML = "";
+		var option = new Option("none", "none");
+		char.appendChild(option);
+		var game = document.getElementById('game').value;
+		for (const [key, value] of Object.entries(json[game])) {
+			var option = new Option(key, key);
+			char.appendChild(option);
+		}
+		char.addEventListener('change', function() {
+			updateAlts(document.getElementById("player" + i + "char").value, document.getElementById("player" + i + "alt"));
+		});
+		updateAlts(document.getElementById("player" + i + "char").value, document.getElementById("player" + i + "alt"));
+	}
+}
+
+function updateAlts(char, alt) {
+	alt.innerHTML = "";
+	var option = new Option("none", "none");
+	alt.appendChild(option);
+	var game = document.getElementById('game').value;
+	if(json[game][char]) {
+		for (const [key, value] of Object.entries(json[game][char])) {
+			var option = new Option(value, value);
+			alt.appendChild(option);
+		}
+	}
+}
