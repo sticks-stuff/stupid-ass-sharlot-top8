8e5b98f8c84f84722d686d27033624506afa82ef
charlotte <charlottemackenzi@gmail.com>
2024-11-04 04:19:33 +1300

    kill all files >25mb please god just build this stupid thing

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 if [ "$CF_PAGES_BRANCH" == "feat/streamhelperassets" ]; then
   git clone https://github.com/joaorb64/StreamHelperAssets --depth 1 -b main
   rm -rf StreamHelperAssets/scripts
+  find StreamHelperAssets -type f -size +25M -exec rm -f {} \;
   node generatePath.js
 else
   node generateJsonAlts.js
