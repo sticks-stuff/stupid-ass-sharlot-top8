7ad4f1f19701d0c05b7819e382b972d97041a3bd
charlotte <charlottemackenzi@gmail.com>
2024-11-04 03:54:49 +1300

    do not checkout history

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 # !/bin/bash
 
 if [ "$CF_PAGES_BRANCH" == "feat/streamhelperassets" ]; then
-  git clone https://github.com/joaorb64/StreamHelperAssets
+  git clone https://github.com/joaorb64/StreamHelperAssets --depth 1 -b main
   node generatePath.js
 else
   node generateJsonAlts.js
