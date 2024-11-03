d91abe0bfef34201afead524ca7e1d89ae8167e7
charlotte <charlottemackenzi@gmail.com>
2024-11-04 04:07:43 +1300

    delete big thing

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 if [ "$CF_PAGES_BRANCH" == "feat/streamhelperassets" ]; then
   git clone https://github.com/joaorb64/StreamHelperAssets --depth 1 -b main
+  rm -rf StreamHelperAssets/scripts
   node generatePath.js
 else
   node generateJsonAlts.js
