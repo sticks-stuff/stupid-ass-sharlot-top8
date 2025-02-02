a647bd0263015245ecfbefccc9d0dfaef1042a64
charlotte <charlottemackenzi@gmail.com>
2025-02-02 21:18:26 +1300

    accidentally committed dev stuff

--- canvas.js
+++ canvas.js
@@ -461,8 +461,8 @@ function handleImageOnload(i, imagesToLoad, img, char = false, alt = false, isSe
 					}
 					
 					var offScreenContainer = document.createElement("div");
-					// offScreenContainer.style.position = "absolute";
-					// offScreenContainer.style.left = "-9999px";
+					offScreenContainer.style.position = "absolute";
+					offScreenContainer.style.left = "-9999px";
 					offScreenContainer.appendChild(char);
 					document.body.appendChild(offScreenContainer);
 	
@@ -493,7 +493,7 @@ function handleImageOnload(i, imagesToLoad, img, char = false, alt = false, isSe
 									ctx.drawImage(shadowCanvas.canvas, POS[i][0] - 2, POS[i][1] - 2, SIZE_SQUARE[i], SIZE_SQUARE[i]);
 								}
 								ctx.drawImage(canvas, POS[i][0], POS[i][1], SIZE_SQUARE[i], SIZE_SQUARE[i]);
-								// offScreenContainer.remove();
+								offScreenContainer.remove();
 								if (isSecondaries) {
 									imagesToLoad.made++;
 									if(imagesToLoad.made >= imagesToLoad.toMake) {
@@ -621,10 +621,7 @@ function go() {
 				};
 				reader.readAsDataURL(charImgInput.files[0]);
 			} else {
-				var fetchPackConfig = await fetch(`https://raw.githack.com/joaorb64/StreamHelperAssets/main/games/${game}/${pack}/config.json`);
-				if (game == "pplus" && pack == "portrait") {
-					fetchPackConfig = await fetch(`./config.json`);
-				}
+				const fetchPackConfig = await fetch(`https://raw.githack.com/joaorb64/StreamHelperAssets/main/games/${game}/${pack}/config.json`);
 				var packConfig = await fetchPackConfig.json();
 				const fetchGameConfig = await fetch(`https://raw.githack.com/joaorb64/StreamHelperAssets/main/games/${game}/base_files/config.json`);
 				gameConfig = await fetchGameConfig.json();
